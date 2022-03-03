17dfba74e0756463d497cf539ec9dbef312220d7
Denis <jujojq@gmail.com>
2022-03-03 12:32:48 +0300

    backend p3

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,4 +1,35 @@
 const express = require("express");
+const User = require("../models/User");
+const auth = require("../middleware/auth.middleware");
 const router = express.Router({ mergeParams: true });
+
+router.patch("/:userId", async (req, res) => {
+  try {
+    const { userId } = req.params;
+    if (userId) {
+      const updatedUser = await User.findByIdAndUpdate(userId, req.body, {
+        new: true,
+      });
+      res.send(updatedUser);
+    } else {
+      res.status(401).json({ message: "Unathorized" });
+    }
+  } catch (e) {
+    res.status(500).json({
+      message: "На сервере произошла ошибка. Попробуйте позже.",
+    });
+  }
+});
+
+router.get("/", auth, async (req, res) => {
+  try {
+    const list = await User.find();
+    res.send(list);
+  } catch (e) {
+    res.status(500).json({
+      message: "На сервере произошла ошибка. Попробуйте позже.",
+    });
+  }
+});
 
 module.exports = router;
